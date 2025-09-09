8d3213060e24d069e6e0a5bcc6e2a9dcf629eef4
NikolayM1 <nmerenko07@gmail.com>
2025-09-09 16:40:19 +0000

    I am commiting this program.

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,13 +1,18 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * TODO: Nikolay Merenko
+ * TODO: September 9th, 2025
+ * TODO: Period 6
+ * TODO: A program that prompts the user to enter their first name, which then promptly greets the person whose name was entered.
  */
-public class HelloWorld {
 
-	public static void main(String[] args) {
-		//TODO: Implement Program Requirements Here
-	}
+import java.util.Scanner;
 
-}
+public class HelloWorld { //class begins
+	public static void main(String[] args) { //method begins
+		Scanner in = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+		String firstName = in.nextLine();
+		System.out.println("Hello," + firstName);
+	} //method ends
+
+} //class ends
